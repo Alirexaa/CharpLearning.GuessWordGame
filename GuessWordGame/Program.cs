3fb00ca140c96f38f1e009871375891788770403
alirexaa <alireza.baloochi1380@gmail.com>
2023-03-06 23:14:07 +0330

    Add project files.

---
+++ GuessWordGame/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace GuessWordGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretKey = "Alireza";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3;
+            bool outOfGuesses = false;
+
+            while (secretKey != guess && !outOfGuesses)
+            {
+                if (guessCount < guessLimit)
+                {
+                    Console.Write("Enter Word: ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                }
+                else
+                {
+                    outOfGuesses = true;
+                }
+            }
+
+            if (outOfGuesses)
+            {
+                Console.WriteLine("you lose");
+            }
+            else
+            {
+                Console.WriteLine("you win");
+            }
+        }
+    }
+}
